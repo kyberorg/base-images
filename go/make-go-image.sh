15f9c6d948b6fcb3e8233cb32c9984bf6b606a68
Aleksandr Muravja <alex@kyberorg.io>
2022-08-03 13:54:42 +0300

    Clean builds without cache only for abi images

--- go/make-go-image.sh
+++ go/make-go-image.sh
@@ -1,5 +1,5 @@
 for tag in latest 1.18 1.18.5; do
-     docker build --no-cache -f Dockerfile -t kio.ee/base/go:"${tag}" .
+     docker build -f Dockerfile -t kio.ee/base/go:"${tag}" .
      docker push kio.ee/base/go:${tag}
      docker rmi -f kio.ee/base/go:${tag}
 done
